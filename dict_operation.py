d0beab23a85f3945081ebee166e592d540d55882
harish-gannoju <gannojuharish@gmail.com>
2022-03-10 19:36:14 -0500

    Update dict_operation.py

--- dict_operation.py
+++ dict_operation.py
@@ -4,10 +4,9 @@ def high_value_key(ex_dict): # should return key for highest value
     max_value = 0
     for u,(x,y) in enumerate(ex_dict.items()):
         for i in list(ex_dict.values())[u:]:
-            while y >= i and y > max_value:
+            if y >= i and y > max_value:
                 max_value = y
                 high_key = x
-                break
                 
     return high_key        
         
